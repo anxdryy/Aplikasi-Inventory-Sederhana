cd9a849d6a44b3821753591d6d523870b08d334a
anxdryy <andry.satriya04@gmail.com>
2026-08-03 09:41:48 +0700

    feat: Menambahkan fillable dan relasi pada Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = ['kode_produk', 'nama_produk', 'satuan', 'stok', 'harga'];
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
